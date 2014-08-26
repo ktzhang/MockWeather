51ded66269a0ff89ec67c67d67c069f97a62ed11
Kevin <kezhang@yahoo-inc.com>
2014-08-26 10:46:47 -0700

    fixed JSON

--- mockwarning.php
+++ mockwarning.php
@@ -11,8 +11,9 @@
 
         // Check connection
         if (mysqli_connect_errno()) {
-            exit;
-          // echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          exit;
+
         }
         
 
