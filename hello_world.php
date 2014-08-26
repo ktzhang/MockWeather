27e20326f2b09ed223e28cdfdf966ce803a0a343
Kevin <kezhang@yahoo-inc.com>
2014-08-26 10:40:47 -0700

    a

--- hello_world.php
+++ hello_world.php
@@ -21,7 +21,8 @@ $con->set_charset('utf8');
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	exit;
-  
+} else {
+	echo "hi";
 }
 
 //Setting table name
